8f964941a1cb16f4e137a632677794e87586ed63
anikotaro <rmakotaro@gmail.com>
2017-01-24 09:50:41 +0900

    標準入力追加

--- VECZO/main.py
+++ VECZO/main.py
@@ -2,17 +2,33 @@
 # -*- coding: utf-8 -*-
 
 from gensim import models
+import sys
+import codecs
 
-sentence = models.doc2vec.LabeledSentence(
-    words=[u'犬',u'今日',u'吠えた'], tags=["SENT_0"])
-sentence1 = models.doc2vec.LabeledSentence(
-    words=[u'猫', u'明日', u'吠えた'], tags=["SENT_1"])
-sentence2 = models.doc2vec.LabeledSentence(
-    words=[u'今', u'猫', u'魚'], tags=["SENT_2"])
-sentence3 = models.doc2vec.LabeledSentence(
-    words=[u'魚', u'泳ぐ', u'海'], tags=["SENT_3"])
 
-sentences = [sentence, sentence1, sentence2, sentence3]
+article_list = []
+for line in sys.stdin:
+    itemlist = line.rstrip().decode('utf-8').split(' ')
+    article_list.append(itemlist)
+
+sentences = []    
+count = 0
+for item in article_list:
+    model = models.doc2vec.LabeledSentence(words=item, tags=['sent_%s' % count])
+    sentences.append(model)
+    count += 1
+
+
+#sentence = models.doc2vec.LabeledSentence(
+#    words=[u'犬',u'今日',u'吠えた'], tags=["SENT_0"])
+#sentence1 = models.doc2vec.LabeledSentence(
+#    words=[u'猫', u'明日', u'吠えた'], tags=["SENT_1"])
+#sentence2 = models.doc2vec.LabeledSentence(
+#    words=[u'今', u'猫', u'魚'], tags=["SENT_2"])
+#sentence3 = models.doc2vec.LabeledSentence(
+#    words=[u'魚', u'泳ぐ', u'海'], tags=["SENT_3"])
+#sentences = [sentence, sentence1, sentence2, sentence3]
+print sentences
 
 class LabeledLineSentence(object):
     def __init__(self, filename):
@@ -43,8 +59,9 @@ model_loaded = models.Doc2Vec.load('my_model.doc2vec')
 # ある単語に類似した単語を取得
 #print (model.similar_by_word(u"魚"))
 
-print (model.most_similar(positive=[u"猫", u"魚"]))
+#print (model.most_similar(positive=[u"猫", u"魚"]))
 #
+print (model.most_similar(positive=[u"魚"]))
 #test_1 = model.similarity(u"猫", u"魚")
 #print(test_1)
 

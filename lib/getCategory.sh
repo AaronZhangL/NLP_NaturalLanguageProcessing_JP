2cae3a0e450790f3acd413008e092d909fc625c1
suzukiiichiro <suzuki.iichiro@gmail.com>
2017-04-06 10:04:56 +0900

    add lib/getCategory.sh

--- lib/getCategory.sh
+++ lib/getCategory.sh
@@ -1,13 +1,12 @@
 
-#real    0m44.887s
-#user    0m32.204s
-#sys     0m18.215s
+# 日本語
 function getCategory(){
+  local DIC="$1" TAG="$2" ;
   l=$(echo "$KEYS_RESULT_LINE"|sed -e "s/<KEYS>//g" -e "s/<\/KEYS>//g" -e "s/<KEY>//g" -e "s/<\/KEY>/\n/g" -e "s/<SCORE>/:/g" -e "s/<\/SCORE>//g"); 
   hn=$(echo "$l"|head -n1|awk -F: '{ print $2; }');
   tn=$(echo "$l"|tail -n1|awk -F: '{ print $2; }') ;
   av=$(echo "$hn" "$tn"|awk '{ print ($1+$2)/2; }') ;
-  echo "###average : $av" ;
+#  echo "###average : $av" ;
   kw=$(echo "$l"|awk -F: '{ if($2> '"$av"'){ print $1; } }');
   kwl=$(echo "$l"|awk -F: '{ print $1; }');
   wcl=$(echo "$l" |wc -l|awk '{ print $1; }') ;
@@ -15,9 +14,9 @@ function getCategory(){
   f=0;
   NPCATEGORY_RESULT_LINE=$(echo "$kwl" | tr ' ' '\n' | \
       while read l; do 
-        echo "#### $l #####" ;
+#        echo "#### $l #####" ;
         ((c==0))&&{ 
-          rl=$(sary "$l" lib/NEWSPACKDB2);
+          rl=$(sary "$l" lib/"$DIC");
 #echo "$rl" | grep -v "^$" | wc -l  ;exit ;
         }||{ 
           rl_t=$(echo "$rl"|fgrep "$l"); 
@@ -30,7 +29,7 @@ function getCategory(){
               sed -e "s/^.*<CATE>//g" -e "s/<\/CATE>.*$//g"| \
               sort|uniq -c|sort -nr| \
               head -n1|awk '{ print $2;}'| \
-              sed -e "s/^/<CATEGORY>/g" -e "s/$/<\/CATEGORY>/g" ;
+              sed -e "s/^/<$TAG>/g" -e "s/$/<\/$TAG>/g" ;
             }
             echo "<SIM>$rl</SIM>" ; break ;  
           }||{
@@ -44,7 +43,7 @@ function getCategory(){
             sed -e "s/^.*<CATE>//g" -e "s/<\/CATE>.*$//g"| \
             sort|uniq -c|sort -nr| \
             head -n1|awk '{ print $2;}'| \
-            sed -e "s/^/<CATEGORY>/g" -e "s/$/<\/CATEGORY>/g" ;
+            sed -e "s/^/<$TAG>/g" -e "s/$/<\/$TAG>/g" ;
             f=1;
         }
         ((c==wcl))&&{

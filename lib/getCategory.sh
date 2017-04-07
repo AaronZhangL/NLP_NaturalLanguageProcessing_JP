7e5facc6caf332289b38877ca29fc48f5172a309
anikotaro <rmakotaro@gmail.com>
2017-04-07 16:52:07 +0900

    wn

--- lib/getCategory.sh
+++ lib/getCategory.sh
@@ -1,5 +1,100 @@
 
 # 日本語
+function  getSynLinks(){
+  local sense="$1";
+  local synset=`echo "$sense"|awk -F\| '{print $1;}'`
+  local link="$2";
+  rst=`sqlite3 "$DB" "select * from synlink where synset1='$synset' and link='$link'"`;
+  echo "$rst";
+}
+function getSynset(){
+  local synset="$1";
+  rst=`sqlite3 "$DB" "select * from synset where synset='$synset'"`;
+  echo "$rst";
+}
+function getSynLinksRecursive(){
+  local sense=$1;
+  local link=$2;
+  local lang="jpn";
+  if [ -n "$3" ];then
+    lang="$3";  
+  fi
+  local depth=$4;
+  synLinks=`getSynLinks "$sense" "$link"`;
+  #echo "sense:$sense link :$link synLinks:$synLinks";
+  if [ -z "$synLinks" ];then
+    continue;
+  fi
+    local space="";
+    for i in `seq 0 $depth`;do
+      space="$space ";
+    done
+    local wordid=`echo "$sense"|awk -F\| '{print $2;}'`;
+    local lemma=`getWord "$wordid"|awk -F\| '{print $3;}'`;
+    local synset=`echo "$sense"|awk -F\| '{print $1;}'`;
+    local name=`getSynset "$synset"|awk -F\| '{print $3;}'`
+    #echo "<sy>$synset</sy><jp>$lemma</jp><en>$name</en>";
+    echo "$lemma";
+    #echo "$space$lemma $name";
+  _senses=`echo "$synLinks"|head -n1|awk -F\| '{print $2;}'|while read synset2;do
+    getSense "$synset2" "$lang";
+    
+  done|grep -v ^$`;
+  depth=$(($depth + 1));
+  echo "$_senses"|head -n1|while read _sense;do
+    getSynLinksRecursive "$_sense" "$sl" "$lg" "$depth"; 
+  done
+}
+function getWord(){
+  local wordid="$1";
+  rst=`sqlite3 "$DB" "select * from word where wordid='$wordid'"`;
+  echo "$rst";
+}
+function getSense(){
+  local rst="";
+  local synset="$1";
+  local lang="jpn";
+  if [ -n "$2" ];then
+    lang="$2";  
+  fi
+  if [ -n "$3" ];then
+    local wordid="$3";
+    rst=`sqlite3 "$DB" "select * from sense where synset='${synset}' and lang='$lang' and wordid='$wordid'"`;
+  fi
+  if [ -z "$rst" ];then
+    rst=`sqlite3 "$DB" "select * from sense where synset='${synset}' and lang='$lang'"`;
+  fi
+  if [ -z "$rst" ];then
+    rst=`sqlite3 "$DB" "select * from sense where synset='${synset}' and lang='eng'"`;
+  fi
+  
+  if [ -z "$rst" ];then
+    synset=`echo "$synset"|sed -e "s|-n||"`;
+    rst=`sqlite3 "$DB" "select * from sense where synset like '${synset}-%' and lang='$lang'"`;
+  fi
+  if [ -z "$rst" ];then
+    rst=`sqlite3 "$DB" "select * from sense where synset like '${synset}-%' and lang='eng'"`;
+  fi
+  echo "$rst";
+}
+function wnquery_byid(){
+  sw=$1;
+  sl=$2;
+  lg=$3;
+  DB="lib/$DIC";
+  wt=`sqlite3 "lib/wnjpn.db" "SELECT * FROM word LEFT JOIN sense ON word.wordid = sense.wordid WHERE lemma = '$sw' AND word.pos = 'n' AND sense.lang = '$lg'"|head -n 1`;
+#179412|jpn|大学||n|08278324-n|179412|jpn||||hand
+  if [ -z "$wt" ];then
+    break;
+  fi
+  wd=`echo "$wt"|awk -F\| '{print $3;}'`;
+  si=$(echo "$wt" | awk -F\| '{print $6}');
+  #228553|jpn|野球||n
+  #00476140-n|44534|eng|0|1|0|eng-30
+  getSense "$si" "$lg" "$wd"|head -n1|while read sense;do
+    getSynLinksRecursive "$sense" "$sl" "$lg" "0";
+  done
+}
 function getCategoryNP(){
   local DIC="$1" TAG="$2" ;
   f=0;
@@ -64,7 +159,24 @@ function getCategoryGT(){
     );
 }
 function getCategoryWN(){
-:
+    #echo "$kwl" | nkf -e | mecab -Owakati | nkf -w | tr ' ' '\n' | grep -v "^$" | grep -v '^...$' | \
+    WNCATEGORY_RESULT_LINE=$( echo "$kwl" | nkf -e | mecab -Owakati | nkf -w | tr ' ' '\n' | grep -v "^$" | grep -v '^...$' | \
+      while read l; do 
+   #echo "$DIC"
+   #echo "#RRR###l $l  #####" ;
+        rl=$(wnquery_byid "$l" "hype" "jpn");
+   #echo "####rl#$rl" ;
+      [[ -z "$rl" ]]&&{
+        :
+      }||{
+          echo "$rl" | \
+          tail -r | \
+          tr "\n" "-" | \
+          sed -e "s/^/<$TAG>/g" -e "s/-$/<\/$TAG>\n/g" ;
+          #break ;
+      }
+    done | sort | uniq -c | sort -nr | awk '{print $2;}' |head -n5;
+    );
 }
 function getCategory(){
   local DIC="$1" TAG="$2" ;
